682e09a77eb80d69a0c5f3f37ba9f12a046a7e22
Hendrix Tavarez <htavarez@gmail.com>
2022-09-02 16:04:09 -0400

    binary

---
+++ binary_overflow/Demo.java
@@ -0,0 +1,10 @@
+public class Demo {
+  public static void main(String[] args) {
+    short a = 32000;
+    short b = 32000;
+    short c = (short) (a + b);
+
+    System.out.println(a + " + " + b + " = " + c);
+
+  }
+}
