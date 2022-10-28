5b03f5e1d7a515d7cb5bf3b2b5fdba186bf6ce9d
Hendrix Tavarez <htavarez@gmail.com>
2022-10-27 22:41:11 -0400

    fixed

--- mips/102722/mips4.asm
+++ mips/102722/mips4.asm
@@ -21,19 +21,21 @@ lf: .asciiz "\n"
 	addi $t0, $t0, 4 # index
 	sw $s1, array($t0)
 	
-	# a[1] = 10
+	# a[2] = 15
 	addi $s2, $zero, 15
 	addi $t0, $t0, 4 # index
 	sw $s2, array($t0)
 	
-	# print array
+  # reset index to 0
 	addi $t0, $zero, 0
+
 while:
 	beq $t0, 12, exit
 	
+  # store the value in the current index to $t9
 	lw $t9, array($t0)
-	addi $t0, $t0, 4
 	
+  # print value
 	li $v0, 1
 	addi $a0, $t9, 0
 	syscall
@@ -42,6 +44,8 @@ while:
 	la $a0, lf
 	syscall
 	
+  # increment index
+	addi $t0, $t0, 4
 	
 	j while
 	
